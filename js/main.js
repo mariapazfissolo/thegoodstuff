77b84e649a60ba0d960be0bf47af5070667cd846
maria paz <mariapaz_fissolo@hotmail.com>
2024-01-08 13:54:27 -0300

    Cambios dark mode

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const colorModeButton = document.querySelector("#color-mode");
+const body = document.body;
+
+colorModeButton.addEventListener("click", cambiarModoColor);
+function cambiarModoColor() {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        colorModeButton.innerText = "Light Mode";
+    } else {
+        colorModeButton.innerText = "Dark Mode";
+    }
+}
+
